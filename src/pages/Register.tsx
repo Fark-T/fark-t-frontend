0a365f54c3bd9ca0cb9ef6dccfcf876204bb0c57
Arn <arn.arn3328@gmail.com>
2023-04-30 20:01:49 +0700

    feat: add register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,34 @@
+import axios from "axios";
 import { IoFastFoodOutline } from "react-icons/io5";
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    username: yup.string().required(),
+    password: yup.string().required(),
+    fname: yup.string().required(),
+    lname: yup.string().required(),
+    phone: yup.string().required(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
+
 const Register = () => {
+  const { register, handleSubmit } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data: FormData) => {
+    try {
+      const res = await axios.post("/api/Auth/register", data);
+      console.log(res);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="w-screen min-h-screen bg-[#1C6758] flex flex-col justify-center items-center">
       <div className="flex overflow-hidden flex-row md:w-full w-5/6 max-w-[110rem] md:min-h-[48rem] min-h-[40rem] rounded-2xl shadow">
@@ -16,44 +44,65 @@ const Register = () => {
               <div className="md:text-4xl text-3xl font-bold">Create an account</div>
               <div className="">Already A Member? <Link to={"/login"} className="text-blue-600 font-medium cursor-pointer">Log In</Link></div>
             </div>
-            <form className="flex flex-col">
-              <div className="flex flex-col py-2">
-                <label className="pb-1">Name</label>
+            <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col py-2">
+                <label className="pb-1">First name</label>
                 <input
                   type="text"
-                  name=""
                   id=""
-                  placeholder="Enter your name"
+                  {...register("fname")}
+                  placeholder="Enter your first name"
                   className="rounded-md border-2 md:w-[420px] w-64 h-10 placeholder-shown:border-[#EEF2E6] placeholder: ps-2"
                 />
               </div>
               <div className="flex flex-col py-2">
-                <label className="pb-1">Email</label>
+                <label className="pb-1">Last name</label>
                 <input
                   type="text"
-                  name=""
                   id=""
-                  placeholder="Enter your email"
+                  {...register("lname")}
+                  placeholder="Enter your last name"
+                  className="rounded-md border-2 md:w-[420px] w-64 h-10 placeholder-shown:border-[#EEF2E6] placeholder: ps-2"
+                />
+              </div>
+              <div className="flex flex-col py-2">
+                <label className="pb-1">Username</label>
+                <input
+                  type="text"
+                  id=""
+                  {...register("username")}
+                  placeholder="Enter your username"
                   className="rounded-md border-2 md:w-[420px] w-64 h-10 placeholder-shown:border-[#EEF2E6] placeholder: ps-2"
                 />
               </div>
               <div className="flex flex-col py-2">
                 <label className="pb-1">Password</label>
                 <input
-                  type="text"
-                  name=""
+                  type="password"
                   id=""
+                  {...register("password")}
                   placeholder="*********"
                   className="rounded-md border-2 md:w-[420px] w-64 h-10 placeholder-shown:border-[#EEF2E6] placeholder: ps-2"
                 />
               </div>
-            </form>
+              <div className="flex flex-col py-2">
+                <label className="pb-1">Phone number</label>
+                <input
+                  type="tel"
+                  id=""
+                  pattern="[0-9]{10}"
+                  {...register("phone")}
+                  placeholder="1234567890"
+                  className="rounded-md border-2 md:w-[420px] w-64 h-10 placeholder-shown:border-[#EEF2E6] placeholder: ps-2"
+                />
+              </div>      
             <button
               type="submit"
               className="bg-[#D6CDA4] md:w-[420px] w-64 h-10 rounded-md mt-5 mb-2 font-bold text-white hover:bg-[#FA9884] transition duration-300"
             >
               Create an account
             </button>
+            </form>
           </div>
         </div>
         <div className="bgLogin w-2/3 rounded-r-lg md:block hidden"></div>
